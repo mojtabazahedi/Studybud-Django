8d9dba557ac5b867b6a5108d1a3fd2f6be50d250
Mojtaba-Zahedi-Amiri <mojtaba.zahedi.amiri@gmail.com>
2021-12-27 19:07:32 +0100

    First Commit

---
+++ base/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
+
+    path('', views.home, name='home'),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
+    
+    path('create_room/', views.createRoom, name="create-room"),
+    path('update_room/<str:pk>/', views.updateRoom, name="update_room"),
+    path('delete_room/<str:pk>/', views.deleteRoom, name="delete_room"),
+    path('delete_message/<str:pk>/', views.deleteMessage, name="delete_message"),
+
+    path('update-user/', views.updateUser, name="update-user"),
+
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
+    
+]
---
+++ studybud/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+import base
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('base.urls'))
+  
+]
